968419727161b78cc619614815dceef1fb6ea7b6
Aadarsh <aadarshhanu23@gmail.com>
2023-02-13 23:57:40 +0530

    made changes to prevent duplication

--- misaligned/color_map.js
+++ misaligned/color_map.js
@@ -11,9 +11,9 @@ function print_color_map() {
       maxMajorLength = Math.max(maxMajorLength, majorColors[i].length);
     }
   }
-  for (let i = 0; i < majorColors.length; i++) {
-    for (let j = 0; j < minorColors.length; j++) {
-      const index = i * 5 + (j + 1);
+  for (let k = 0; k < majorColors.length; k++) {
+    for (let l = 0; l < minorColors.length; l++) {
+      const index = k * 5 + (l + 1);
       const indexString = index.toString().padEnd(maxIndexLength, ' ');
       const majorString = majorColors[i].padEnd(maxMajorLength, ' ');
       map.push(`${indexString} | ${majorString} | ${minorColors[j]}`);
